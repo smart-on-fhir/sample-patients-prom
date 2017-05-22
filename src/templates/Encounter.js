0764ac1e268c8fac2285cca2ad3dc2f1bb418433
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2017-05-22 11:12:23 -0400

    Fixed encounter template

--- src/templates/Encounter.js
+++ src/templates/Encounter.js
@@ -1,27 +1,48 @@
-module.exports = data => ({
-    resourceType: "Encounter",
-    id: data[0],
-    status: (data[1] || "").toLowerCase(),
-    class: {
-        code: data[2]
-    },
-    type: [
-        {
-            "coding": [
-                {
-                    "system": "http://snomed.info/sct",
-                    "code"  : "270427003"
-                }
-            ],
-            "text": data[2].charAt(0).toUpperCase() + data[2].substr(1) +  " Encounter"
+module.exports = data => {
+    let out = {
+        resourceType: "Encounter",
+        id: data[0],
+        status: (data[1] || "").toLowerCase(),
+        class: {
+            code: data[2]
+        },
+        type: [
+            {
+                coding: [
+                    {
+                        system : data[7],
+                        code   : data[8],
+                        display: data[9]
+                    }
+                ],
+                text: data[9]
+            }
+        ],
+        subject: {
+            reference: data[3],
+            display: data[4]
+        },
+        period: {
+            start: data[5],
+            end  : data[6]
         }
-    ],
-    subject: {
-        reference: data[3],
-        display: data[4]
-    },
-    period: {
-        start: data[5],
-        end  : data[6]
+    };
+
+    // reason ------------------------------------------------------------------
+    if (data[10] && data[11] && data[12]) {
+        out.reason = [
+            {
+                coding: [
+                    {
+                        system : data[10],
+                        code   : data[11],
+                        display: data[12]
+                    }
+                ],
+                text: data[12]
+            }
+        ];
     }
-});
+
+    return out;
+}
